cd3c28efa1c94aa7c4e4df71b157ee14e6290120
ASMAEELKHIRAOUI <asmaaekw@gmail.com>
2023-10-18 09:27:51 +0100

    done working on the service side of the event crud

--- src/main/java/com/example/f34tur3s/service/EventService.java
+++ src/main/java/com/example/f34tur3s/service/EventService.java
@@ -1,12 +1,35 @@
 package com.example.f34tur3s.service;
 
+import com.example.f34tur3s.domain.Event;
 import com.example.f34tur3s.repository.EventRepository;
+
+import java.util.Date;
+import java.util.List;
 
 public class EventService {
 
-//    private final EventRepository eventRepository;
-//
-//    public EventService() {
-//        this.eventRepository = new EventRepository();
-//    }
+    private final EventRepository eventRepository;
+
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    public Event createEvent(Event event){
+        validate(event);
+        return eventRepository.save(event);
+    }
+
+    public List<Event> getAllEvents(){
+        return eventRepository.findAll();
+    }
+
+    private void validate(Event event){
+        Date today = new Date();
+        if (event.getName().isBlank() || event.getLocation().isBlank() || event.getDescription().isBlank()){
+            throw new IllegalArgumentException("All fields needed");
+        }
+        if (event.getDate().before(today)){
+            throw new IllegalArgumentException("Date not valid");
+        }
+    }
 }
