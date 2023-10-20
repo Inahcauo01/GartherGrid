b4d44db0aa39e7b77ad60f7be4cfa13d0168c159
fouad <fouadwork2@gmail.com>
2023-10-20 12:20:56 +0100

    add registration and login

---
+++ src/main/java/com/example/f34tur3s/repository/UserRepository.java
@@ -0,0 +1,45 @@
+package com.example.f34tur3s.repository;
+
+import com.example.f34tur3s.domain.User;
+import com.example.f34tur3s.utils.EntityManagerUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class UserRepository {
+    private final EntityManager entityManager;
+
+    public UserRepository() {
+        entityManager = EntityManagerUtil.getEntityManager();
+    }
+
+    public void save(User user) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(user);
+        entityManager.getTransaction().commit();
+    }
+
+    public Stream<User> findByEmail(String email) {
+        Stream<User> userStream= entityManager.createQuery("SELECT u FROM User u WHERE u.email = :email")
+                .setParameter("email", email).getResultStream();
+        return userStream;
+    }
+
+    public User findById(Long id) {
+        /*EntityManager entityManager = entityManagerFactory.createEntityManager();*/
+        User user = entityManager.find(User.class, id);
+       /* entityManager.close();*/
+        return user;
+    }
+
+
+    public List<User> getAllUsers() {
+        List<User> users = entityManager.createQuery("SELECT u FROM User u", User.class).getResultList();
+        return users;
+    }
+
+}
